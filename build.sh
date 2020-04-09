6680fa6f18939f9532e0d40b2c245a85b6704081
Issam Maghni <me@concati.me>
2020-04-09 18:08:02 -0400

    Replace bashism by POSIX shell

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-${CC:${cc:gcc}} -std=c89 -pedantic -Wall -Wextra -O3 -I inc/ -o fe src/fe.c src/main.c
+#!/bin/sh
+CC=${CC:-cc}
+command -v $CC >/dev/null || CC=gcc
+$CC -std=c89 -pedantic -Wall -Wextra -O3 -I inc/ -o fe src/fe.c src/main.c
