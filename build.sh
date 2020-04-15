648568ef5941d8728ebb73a2513ab141471f2104
Issam Maghni <me@concati.me>
2020-04-15 01:19:12 -0400

    Use ‘exec’ in build.sh to replace process

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 CC=${CC:-cc}
 command -v $CC >/dev/null || CC=gcc
-$CC -std=c89 -pedantic -Wall -Wextra -O3 -I inc/ -o fe src/fe.c src/main.c
+exec $CC -std=c89 -pedantic -Wall -Wextra -O3 -I inc/ -o fe src/fe.c src/main.c
